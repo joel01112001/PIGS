0f9f424871cb98b1e53aa13c01f4c5b57cd6cfcf
PabloSeguraLopez <pabseglop@gmail.com>
2025-03-21 20:19:55 +0000

    Changes done on the search page

---
+++ src/app/interfaces/Offer.ts
@@ -0,0 +1,8 @@
+export class Offer {
+  title: string = 'Title';
+  description: string = "Description";
+  employer: string = "";
+  employee: string = "";
+  salary: number = 0;
+  category: string = "";
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, RouterOutlet } from '@angular/router';
 import { routes } from './app/app.routes';
 import { NavComponent } from './app/components/nav/nav.component';
+import {Offer} from './app/interfaces/Offer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, Offer],
   template: `
     <app-nav></app-nav>
     <main>
